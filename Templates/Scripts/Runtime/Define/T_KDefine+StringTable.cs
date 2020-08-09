0d6ca6f72ab08dcd2fe8d747ccd05b719e864beb
dante <are2341@nate.com>
2020-08-10 00:12:27 +0900

    [이상동] 테이블 관련 템플릿 수정

--- Templates/Scripts/Runtime/Define/T_KDefine+StringTable.cs
+++ Templates/Scripts/Runtime/Define/T_KDefine+StringTable.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 //! 문자열 테이블 상수
 public static partial class KDefine {
 	#region 기본
-
+	public const string ST_KEY_ALERT_P_TITLE = "ALERT_P_TITLE";
+	public const string ST_KEY_ALERT_P_MSG = "ALERT_P_MSG";
+	public const string ST_KEY_ALERT_P_OK_BTN_TEXT = "ALERT_P_OK_BTN_TEXT";
+	public const string ST_KEY_ALERT_P_CANCEL_BTN_TEXT = "ALERT_P_CANCEL_BTN_TEXT";
 	#endregion			// 기본
 }
 #endif			// #if NEVER_USE_THIS
