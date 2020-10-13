9a0a54c64884efc138ec03af8d0798cd9384d8bf
sd.lee <sd.lee@ninetap.com>
2020-10-13 19:23:25 +0900

    [이상동] 게임 정보 저장소 스크립트 수정

--- Templates/Scripts/Runtime/Storage/T_CGameInfoStorage.cs
+++ Templates/Scripts/Runtime/Storage/T_CGameInfoStorage.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 using MessagePack;
 
 #if NEVER_USE_THIS
-//! 유저 정보
+//! 게임 정보
 [MessagePackObject]
 [System.Serializable]
-public sealed class CUserInfo : CBaseInfo {
+public sealed class CGameInfo : CBaseInfo {
 	#region 함수
 	//! 생성자
-	public CUserInfo() : base(KDefine.B_VERSION_USER_INFO) {
+	public CGameInfo() : base(KDefine.B_VERSION_GAME_INFO) {
 		// Do Nothing
 	}
 	#endregion			// 함수
 }
 
-//! 유저 정보 저장소
-public class CUserInfoStorage : CSingleton<CUserInfoStorage> {
+//! 게임 정보 저장소
+public class CGameInfoStorage : CSingleton<CGameInfoStorage> {
 	#region 프로퍼티
-	public CUserInfo UserInfo { get; private set; } = null;
+	public System.DateTime PrevAdsTime { get; set; } = System.DateTime.Now;
+	public CGameInfo GameInfo { get; private set; } = null;
 	#endregion			// 프로퍼티
 
 	#region 함수
@@ -32,35 +33,35 @@ public class CUserInfoStorage : CSingleton<CUserInfoStorage> {
 
 	//! 상태를 리셋한다
 	public virtual void Reset() {
-		this.UserInfo = new CUserInfo();
+		this.GameInfo = new CGameInfo();
 	}
 
-	//! 유저 정보를 저장한다
-	public void SaveUserInfo() {
-		this.SaveUserInfo(KDefine.B_DATA_PATH_USER_INFO);
+	//! 게임 정보를 저장한다
+	public void SaveGameInfo() {
+		this.SaveGameInfo(KDefine.B_DATA_PATH_GAME_INFO);
 	}
 
-	//! 유저 정보를 저장한다
-	public void SaveUserInfo(string a_oFilepath) {
-		this.SaveUserInfo(KDefine.B_DATA_PATH_USER_INFO);
+	//! 게임 정보를 저장한다
+	public void SaveGameInfo(string a_oFilepath) {
+		CFunc.WriteMsgPackObj(a_oFilepath, this.GameInfo);
 	}
 
-	//! 유저 정보를 로드한다
-	public void LoadUserInfo() {
-		this.LoadUserInfo(KDefine.B_DATA_PATH_USER_INFO);
+	//! 게임 정보를 로드한다
+	public void LoadGameInfo() {
+		this.LoadGameInfo(KDefine.B_DATA_PATH_GAME_INFO);
 	}
 
-	//! 유저 정보를 로드한다
-	public void LoadUserInfo(string a_oFilepath) {
+	//! 게임 정보를 로드한다
+	public void LoadGameInfo(string a_oFilepath) {
 		// 파일이 존재 할 경우
 		if(File.Exists(a_oFilepath)) {
 			try {
-				this.UserInfo = CFunc.ReadMsgPackObj<CUserInfo>(a_oFilepath);
+				this.GameInfo = CFunc.ReadMsgPackObj<CGameInfo>(a_oFilepath);
 			} catch(System.Exception oException) {
-				CFunc.ShowLogWarning("CUserInfoStorage.LoadUserInfo Exception: {0}", oException.Message);
+				CFunc.ShowLogWarning("CGameInfoStorage.LoadGameInfo Exception: {0}", oException.Message);
 
 				this.Reset();
-				this.SaveUserInfo(a_oFilepath);
+				this.SaveGameInfo(a_oFilepath);
 			}
 		}
 	}
