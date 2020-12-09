1c67e3f1270804bbfeeb6057c60a90b55a6ea2f1
dante <are2341@nate.com>
2020-12-09 22:13:42 +0900

    [이상동] 스크립트 템플릿 추가

---
+++ Templates/Scripts/Runtime/Function/T_LogFunc+Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NEVER_USE_THIS
+//! 전역 로그 함수
+public static partial class LogFunc {
+	#region 클래스 함수
+	
+	#endregion			// 클래스 함수
+}
+#endif			// #if NEVER_USE_THIS
