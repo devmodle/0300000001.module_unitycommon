3da7a4600ffe5a088f13b53c4b0c1b684d5fd41c
sd.lee <sd.lee@ninetap.com>
2020-12-14 12:13:28 +0900

    [이상동] 스크립트 템플릿 추가

---
+++ Templates/Scripts/Runtime/Access/T_Access+Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NEVER_USE_THIS
+//! 전역 접근자
+public static partial class Access {
+	#region 클래스 함수
+	
+	#endregion			// 클래스 함수
+}
+#endif			// #if NEVER_USE_THIS
---
+++ Templates/Scripts/Runtime/Extension/T_Extension+Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NEVER_USE_THIS
+//! 전역 확장 클래스
+public static partial class Extension {
+	#region 클래스 함수
+	
+	#endregion			// 클래스 함수
+}
+#endif			// #if NEVER_USE_THIS
