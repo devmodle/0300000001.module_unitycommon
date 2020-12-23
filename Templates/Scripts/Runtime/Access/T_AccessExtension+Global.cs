04ea4ad449a5fb362d0e7d418b613a3eb90876e4
sd.lee <sd.lee@ninetap.com>
2020-12-23 20:12:32 +0900

    [이상동] 스크립트 템플릿 추가

---
+++ Templates/Scripts/Runtime/Access/T_AccessExtension+Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NEVER_USE_THIS
+//! 전역 접근 확장 클래스
+public static partial class AccessExtension {
+	#region 클래스 함수
+	
+	#endregion			// 클래스 함수
+}
+#endif			// #if NEVER_USE_THIS
