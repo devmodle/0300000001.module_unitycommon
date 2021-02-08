50635d4eb962a53e1b4496ed5061b3daf4e1fa49
sd.lee <sd.lee@ninetap.com>
2021-02-08 21:29:25 +0900

    [이상동] 스크립트 템플릿 추가

---
+++ Templates/Scripts/Editor/Importer/T_CSubAssetImporter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NEVER_USE_THIS
+#if UNITY_EDITOR
+using UnityEditor;
+
+//! 서브 에셋 추가자
+[InitializeOnLoad]
+public class CSubAssetImporter : CAssetImporter {
+	#region 함수
+	//! 사운드를 추가했을 경우
+	public override void OnPreprocessAudio() {
+		base.OnPreprocessAudio();
+	}
+
+	//! 텍스처를 추가했을 경우
+	public virtual void OnPreprocessTexture() {
+		base.OnPreprocessTexture();
+	}
+	#endregion			// 함수
+}
+#endif			// #if UNITY_EDITOR
+#endif			// #if NEVER_USE_THIS
