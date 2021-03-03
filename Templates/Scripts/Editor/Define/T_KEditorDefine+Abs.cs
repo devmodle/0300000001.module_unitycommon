f4dd2e75a870f8606a5853dba1b9ff5778c75832
sd.lee <sd.lee@ninetap.com>
2021-03-03 18:03:08 +0900

    [이상동] 에디터 상수 템플리 수정

--- Templates/Scripts/Editor/Define/T_KEditorDefine+Abs.cs
+++ Templates/Scripts/Editor/Define/T_KEditorDefine+Abs.cs
@@ -216,12 +216,12 @@ public static partial class KEditorDefine {
 #endif			// #if FACEBOOK_ENABLE || FACEBOOK_MODULE_ENABLE
 
 #if FIREBASE_ENABLE || FIREBASE_MODULE_ENABLE
-		["com.google.firebase.auth"] = "7.0.2",
-		["com.google.firebase.analytics"] = "7.0.2",
-		["com.google.firebase.crashlytics"] = "7.0.2",
-		["com.google.firebase.database"] = "7.0.2",
-		["com.google.firebase.remote-config"] = "7.0.2",
-		["com.google.firebase.messaging"] = "7.0.2",
+		["com.google.firebase.auth"] = "7.1.0",
+		["com.google.firebase.analytics"] = "7.1.0",
+		["com.google.firebase.crashlytics"] = "7.1.0",
+		["com.google.firebase.database"] = "7.1.0",
+		["com.google.firebase.remote-config"] = "7.1.0",
+		["com.google.firebase.messaging"] = "7.1.0",
 
 		["unitymodule.common.firebase"] = "https://9tap:NT9studio!@gitlab.com/9tapmodule.repository/unitymodule_common_firebase_client.git#1.2.5",
 #endif			// #if FIREBASE_ENABLE || FIREBASE_MODULE_ENABLE
