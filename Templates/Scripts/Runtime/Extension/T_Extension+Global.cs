00fdab1ade0c98c15e7ab0016c81fac8e29c476a
dante <are2341@nate.com>
2021-03-08 13:51:29 +0900

    [이상동] 스크립트 템플릿 수정

--- Templates/Scripts/Runtime/Extension/T_Extension+Global.cs
+++ Templates/Scripts/Runtime/Extension/T_Extension+Global.cs
@@ -6,7 +6,18 @@ using UnityEngine;
 //! 전역 확장 클래스
 public static partial class Extension {
 	#region 클래스 함수
-	
+	//! 효과를 재생한다
+	public static void ExPlay(this ParticleSystem a_oSender, System.Action<CEventDispatcher> a_oCallback, bool a_bIsReset = true, bool a_bIsRemoveChildren = false) {
+		CAccess.Assert(a_oSender != null);
+		var oEventDispatcher = a_oSender.GetComponentInChildren<CEventDispatcher>();
+
+		// 이벤트 전달자가 존재 할 경우
+		if(oEventDispatcher != null) {
+			oEventDispatcher.ParticleCallback = a_oCallback;
+		}
+
+		a_oSender.ExPlay(a_bIsReset, a_bIsRemoveChildren);
+	}
 	#endregion			// 클래스 함수
 }
 #endif			// #if NEVER_USE_THIS
