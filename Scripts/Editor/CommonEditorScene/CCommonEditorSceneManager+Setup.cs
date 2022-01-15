d7dbc2e033fed9a0bc9fc16755362944ef633252
dante <are2341@nate.com>
2022-01-16 07:40:32 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Editor/CommonEditorScene/CCommonEditorSceneManager+Setup.cs
+++ Scripts/Editor/CommonEditorScene/CCommonEditorSceneManager+Setup.cs
@@ -38,12 +38,7 @@ public static partial class CCommonEditorSceneManager {
 			// 메인 광원 일 경우
 			if(oLights[i].name.Equals(KCDefine.U_OBJ_N_SCENE_MAIN_LIGHT)) {
 				oLights[i].type = LightType.Directional;
-
-#if MAIN_LIGHT_REALTIME_LIGHTMAP_BAKE_ENABLE
-				oLights[i].lightmapBakeType = LightmapBakeType.Realtime;
-#else
 				oLights[i].lightmapBakeType = LightmapBakeType.Mixed;
-#endif			// #if MAIN_LIGHT_REALTIME_LIGHTMAP_BAKE_ENABLE
 
 				oLights[i].ExSetTag(KCDefine.U_TAG_MAIN_LIGHT);
 			}
