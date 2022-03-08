3c5e510ac5f0823d2855d555b3136d7b3d6c9cab
sd.lee <sd.lee@ninetap.com>
2022-03-08 12:24:42 +0900

    [이상동] 상수 템플릿 추가

---
+++ Templates/Scripts/Runtime/Define/T_KDefine+SubGlobal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if SCRIPT_TEMPLATE_ONLY
+/** 서브 전역 상수 */
+public static partial class KDefine {
+	#region 기본
+	
+	#endregion			// 기본
+
+	#region 런타임 상수
+
+	#endregion			// 런타임 상수
+}
+#endif			// #if SCRIPT_TEMPLATE_ONLY
