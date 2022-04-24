b5d663f0386ae2b397cd1184a945b5de34101466
sd.lee <sd.lee@ninetap.com>
2022-04-25 05:05:25 +0900

    [이상동] 에디터 스크립트 수정

--- Scripts/Editor/CommonEditorScene/CCommonEditorSceneManager.cs
+++ Scripts/Editor/CommonEditorScene/CCommonEditorSceneManager.cs
@@ -195,8 +195,11 @@ public static partial class CCommonEditorSceneManager {
 						return true;
 					});
 				} finally {
-					string oDirPath = Path.GetDirectoryName(KCEditorDefine.B_ABS_DIR_P_UNITY_PACKAGES);
-					CFunc.WriteStr(string.Format(KCDefine.B_TEXT_FMT_2_COMBINE, oDirPath, KCDefine.B_FILE_EXTENSION_TXT), CCommonEditorSceneManager.m_oStrBuilder.ToString());
+					// 디렉토리가 존재 할 경우
+					if(Directory.Exists(KCEditorDefine.B_ABS_DIR_P_UNITY_PACKAGES)) {
+						string oDirPath = Path.GetDirectoryName(KCEditorDefine.B_ABS_DIR_P_UNITY_PACKAGES);
+						CFunc.WriteStr(string.Format(KCDefine.B_TEXT_FMT_2_COMBINE, oDirPath, KCDefine.B_FILE_EXTENSION_TXT), CCommonEditorSceneManager.m_oStrBuilder.ToString());
+					}
 				}
 
 				// 빌드 가능 할 경우
