099f58b1755ec415316dd5f592fcc44265dc3904
sd.lee <sd.lee@ninetap.com>
2022-06-03 14:23:14 +0900

    [이상동] 스크립트 템플릿 갱신

---
+++ Templates/Scripts/Engine/Base/T_CEPlayableObj.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if SCRIPT_TEMPLATE_ONLY
+#if EXTRA_SCRIPT_MODULE_ENABLE && ENGINE_TEMPLATES_MODULE_ENABLE
+namespace SampleEngineName {
+	/** 플레이 가능 객체 */
+	public partial class CEPlayableObj : CEObj {
+		#region 함수
+		/** 초기화 */
+		public override void Init(STParams a_stParams) {
+			base.Init(a_stParams);
+		}
+		#endregion			// 함수
+	}
+}
+#endif			// #if EXTRA_SCRIPT_MODULE_ENABLE && ENGINE_TEMPLATES_MODULE_ENABLE
+#endif			// #if SCRIPT_TEMPLATE_ONLY
