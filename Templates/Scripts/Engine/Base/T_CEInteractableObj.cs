76997440dbe5748d8b18d427d7eccfdc983e42c4
sd.lee <sd.lee@ninetap.com>
2022-07-12 12:30:30 +0900

    [이상동] 서브 모듈 갱신

--- Templates/Scripts/Engine/Base/T_CEInteractableObj.cs
+++ Templates/Scripts/Engine/Base/T_CEInteractableObj.cs
@@ -7,16 +7,21 @@ using UnityEngine.Events;
 #if SCRIPT_TEMPLATE_ONLY
 #if EXTRA_SCRIPT_MODULE_ENABLE && ENGINE_TEMPLATES_MODULE_ENABLE
 namespace SampleEngineName {
-	/** 플레이 불가능 객체 */
-	public partial class CENonPlayableObj : CEInteractableObj {
+	/** 상호 작용 가능 객체 */
+	public partial class CEInteractableObj : CEObj {
 		// 매개 변수
 		public new partial struct STParams {
-			public CEInteractableObj.STParams m_stBaseParams;
+			public CEObj.STParams m_stBaseParams;
 		}
-
+		
 		#region 변수
 		private STParams m_stParams;
+		private List<STAbilityValInfo> m_oAbilityValInfoList = new List<STAbilityValInfo>();
 		#endregion			// 변수
+
+		#region 프로퍼티
+		public List<STAbilityValInfo> AbilityValInfoList => m_oAbilityValInfoList;
+		#endregion			// 프로퍼티
 
 		#region 함수
 		/** 초기화 */
