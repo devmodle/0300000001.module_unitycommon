9520c059c2e4885483331a99363d557e9e605dbe
dante <are2341@nate.com>
2022-07-30 00:09:47 +0900

    [이상동] 서브 모듈 갱신

--- Templates/01.UnityProject/Scripts/Runtime/Scene/T_CSubMainSceneManager.cs
+++ Templates/01.UnityProject/Scripts/Runtime/Scene/T_CSubMainSceneManager.cs
@@ -25,7 +25,7 @@ namespace MainScene {
 
 		#region 변수
 		private Dictionary<EKey, STIDInfo> m_oIDInfoDict = new Dictionary<EKey, STIDInfo>() {
-			[EKey.SEL_ID_INFO] = default(STIDInfo)
+			[EKey.SEL_ID_INFO] = STIDInfo.INVALID
 		};
 
 		/** =====> UI <===== */
