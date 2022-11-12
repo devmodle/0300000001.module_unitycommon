4e9afc3d25508a07d3cb2da87bf4a3666ea95e60
dante <lkstudio.dante@gmail.com>
2022-11-12 13:50:00 +0900

    [이상동] 코드 형식 갱신

--- Scripts/Runtime/AgreeScene/CAgreeSceneManager.cs
+++ Scripts/Runtime/AgreeScene/CAgreeSceneManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-namespace AgreeScene
-{
+namespace AgreeScene {
 	/** 약관 동의 씬 관리자 */
-	public abstract partial class CAgreeSceneManager : CSceneManager
-	{
+	public abstract partial class CAgreeSceneManager : CSceneManager {
 		#region 프로퍼티
 		public override string SceneName => KCDefine.B_SCENE_N_AGREE;
 
@@ -19,32 +17,27 @@ namespace AgreeScene
 
 		#region 함수
 		/** 초기화 */
-		public override void Awake()
-		{
+		public override void Awake() {
 			base.Awake();
 
 			// 초기화 되었을 경우
-			if(CSceneManager.IsInit)
-			{
+			if(CSceneManager.IsInit) {
 				CSceneManager.GetSceneManager<StartScene.CStartSceneManager>(KCDefine.B_SCENE_N_START)?.gameObject.ExSendMsg(string.Empty, KCDefine.SS_FUNC_N_START_SCENE_EVENT, EStartSceneEvent.LOAD_AGREE_SCENE, false);
 			}
 		}
 
 		/** 초기화 */
-		public sealed override void Start()
-		{
+		public sealed override void Start() {
 			base.Start();
 
 			// 초기화 되었을 경우
-			if(CSceneManager.IsInit)
-			{
+			if(CSceneManager.IsInit) {
 				StartCoroutine(this.CoStart());
 			}
 		}
 
 		/** 다음 씬을 로드한다 */
-		protected void LoadNextScene()
-		{
+		protected void LoadNextScene() {
 			CCommonAppInfoStorage.Inst.AppInfo.IsAgree = true;
 			CCommonAppInfoStorage.Inst.SaveAppInfo();
 
@@ -58,8 +51,7 @@ namespace AgreeScene
 		protected abstract void ShowEUAgreePopup(string a_oPrivacyURL, string a_oServicesURL);
 
 		/** 초기화 */
-		private IEnumerator CoStart()
-		{
+		private IEnumerator CoStart() {
 			yield return CFactory.CoCreateWaitForSecs(KCDefine.U_DELAY_INIT);
 			this.SetupActiveScene();
 
@@ -69,15 +61,11 @@ namespace AgreeScene
 			bool bIsAgree = CCommonAppInfoStorage.Inst.AppInfo.IsAgree;
 
 			// 약관 동의 상태 일 경우
-			if(bIsAgree || !CCommonAppInfoStorage.Inst.IsNeedsAgree(CCommonAppInfoStorage.Inst.CountryCode))
-			{
+			if(bIsAgree || !CCommonAppInfoStorage.Inst.IsNeedsAgree(CCommonAppInfoStorage.Inst.CountryCode)) {
 				this.LoadNextScene();
-			}
-			else
-			{
+			} else {
 				// 한국 일 경우
-				if(CCommonAppInfoStorage.Inst.CountryCode.Equals(KCDefine.B_KOREA_COUNTRY_CODE))
-				{
+				if(CCommonAppInfoStorage.Inst.CountryCode.Equals(KCDefine.B_KOREA_COUNTRY_CODE)) {
 					var oPrivacy = CResManager.Inst.GetRes<TextAsset>(KCDefine.AS_DATA_P_PRIVACY);
 					var oServices = CResManager.Inst.GetRes<TextAsset>(KCDefine.AS_DATA_P_SERVICES);
 
@@ -85,9 +73,7 @@ namespace AgreeScene
 
 					CResManager.Inst.RemoveRes<TextAsset>(KCDefine.AS_DATA_P_PRIVACY, true);
 					CResManager.Inst.RemoveRes<TextAsset>(KCDefine.AS_DATA_P_SERVICES, true);
-				}
-				else
-				{
+				} else {
 					this.ShowEUAgreePopup(CProjInfoTable.Inst.CompanyInfo.m_oPrivacyURL, CProjInfoTable.Inst.CompanyInfo.m_oServicesURL);
 				}
 			}
--- Scripts/Runtime/OverlayScene/COverlaySceneManager.cs
+++ Scripts/Runtime/OverlayScene/COverlaySceneManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-namespace OverlayScene
-{
+namespace OverlayScene {
 	/** 중첩 씬 관리자 */
-	public partial class COverlaySceneManager : CSceneManager
-	{
+	public partial class COverlaySceneManager : CSceneManager {
 		#region 프로퍼티
 		public override string SceneName => KCDefine.B_SCENE_N_OVERLAY;
 
