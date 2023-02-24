2b14581be2ea98837dcffc6f95e0c0ae438206d8
dante <lkstudio.dante@gmail.com>
2023-02-24 13:09:50 +0900

    [이상동] 스크립트 템플릿 갱신

--- Client/Templates/02-SubUnityProject/Scripts/Engine/T_CEngine+Sub.cs
+++ Client/Templates/02-SubUnityProject/Scripts/Engine/T_CEngine+Sub.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 #if EXTRA_SCRIPT_MODULE_ENABLE && UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE
+using System.Linq;
 using UnityEngine.EventSystems;
 
 namespace NSEngine {
@@ -173,7 +174,7 @@ namespace NSEngine {
 			}
 		}
 #endif // #if UNITY_EDITOR
-		#endregion // 조건부 함수
+#endregion // 조건부 함수
 	}
 
 	/** 서브 엔진 - 설정 */
@@ -222,7 +223,15 @@ namespace NSEngine {
 	/** 서브 엔진 - 접근 */
 	public partial class CEngine : CComponent {
 		#region 함수
+		/** 클리어 여부를 검사한다 */
+		public bool IsClear() {
+			return m_oClearTargetInfoDict.All((a_stKeyVal) => a_stKeyVal.Value.m_stValInfo01.m_dmVal <= KCDefine.B_VAL_0_INT);
+		}
 
+		/** 클리어 실패 여부를 검사한다 */
+		public bool IsClearFail() {
+			return this.SelPlayerObj != null && this.SelPlayerObj.AbilityValDictWrapper.m_oDict01.ExGetAbilityVal(EAbilityKinds.STAT_HP_01) <= KCDefine.B_VAL_0_INT;
+		}
 		#endregion // 함수
 	}
 
