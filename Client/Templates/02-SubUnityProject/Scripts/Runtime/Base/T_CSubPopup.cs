72b2086f90920f18822db1d564547c641a81ac11
sd.lee <sd.lee@ninetap.com>
2023-04-19 16:03:00 +0900

    [이상동] 서브 모듈 갱신

--- Client/Templates/02-SubUnityProject/Scripts/Runtime/Base/T_CSubPopup.cs
+++ Client/Templates/02-SubUnityProject/Scripts/Runtime/Base/T_CSubPopup.cs
@@ -14,14 +14,7 @@ public abstract partial class CSubPopup : CPopup {
 		[HideInInspector] MAX_VAL
 	}
 
-#region 프로퍼티
-	public override float ShowTimeScale => KCDefine.B_VAL_0_REAL;
-	public override float CloseTimeScale => KCDefine.B_VAL_1_REAL;
-
-	public override EAniType AniType => EAniType.DROPDOWN;
-#endregion // 프로퍼티
-
-#region 함수
+	#region 함수
 	/** 초기화 */
 	public override void Awake() {
 		base.Awake();
@@ -42,7 +35,7 @@ public abstract partial class CSubPopup : CPopup {
 	private void UpdateUIsState() {
 		// Do Something
 	}
-#endregion // 함수
+	#endregion // 함수
 }
 #endif // #if EXTRA_SCRIPT_MODULE_ENABLE && UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE
 #endif // #if SCRIPT_TEMPLATE_ONLY
