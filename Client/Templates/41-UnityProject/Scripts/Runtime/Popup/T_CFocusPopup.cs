2c165b7ff1fe93728c3ef8d9f84cc1275ecda674
sd.lee <sd.lee@ninetap.com>
2024-01-31 17:13:09 +0900

    [이상동] 서브 모듈 갱신

--- Client/Templates/41-UnityProject/Scripts/Runtime/Popup/T_CFocusPopup.cs
+++ Client/Templates/41-UnityProject/Scripts/Runtime/Popup/T_CFocusPopup.cs
@@ -48,7 +48,7 @@ public partial class CFocusPopup : CSubPopup {
 	/** 초기화 */
 	public override void Awake() {
 		base.Awake();
-		this.SetIsIgnoreAni(true);
+		this.SetIsEnableAni(false);
 
 		// 이미지를 설정한다
 		CFunc.SetupComponents(new List<(EKey, string, GameObject)>() {
